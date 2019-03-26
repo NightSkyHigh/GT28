6c5617bdeb38091fde6051a0d7430c412bf84766
kaaasen <kim.andre.aasen@gmail.com>
2019-03-26 12:19:55 +0100

    Modularized a bit

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -7,25 +7,7 @@
             align-center
           >
             <v-flex text-xs-center>
-              <div id="app">
-                <div id="titleFrame"><h1>Title</h1></div>
-                <div id="outterFrame">
-                  <div id="locationFrame">
-                    <ul>
-                      <li><a href="#">Oslo</a></li>
-                      <li><a href="#">London</a></li>
-                      <li><a href="#">Berlin</a></li>
-                      <li><a href="#">Moscow</a></li>
-                      </li>
-                    </ul>
-                  </div>
-                  <div id="mapFrame">
-                    <div id="mapid"></div>
-                    <script src="map.js">
-                    </script>
-                  </div>
-                </div>
-              </div>
+              
               <v-tooltip left>
                 <template v-slot:activator="{ on }">
                   <v-btn :href="source" icon large target="_blank" v-on="on">
