a12e9e65092ceb06c1b6f4ba2e148405609ddb8d
“NightSkyHigh” <Thomas_95_@hotmail.com>
2019-03-26 09:27:07 +0100

    methods in main

--- src/main.js
+++ src/main.js
@@ -2,7 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+var app = new Vue({
+  el: '#app',
+  data() {
+      return{
 
+          url: "https://www.yr.no/sted/Norge/Oslo/Oslo/Oslo/varsel.xml",
+
+      }
+  }, 
+  
+  methods: {
+    
+  }
+
+/*
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app') 
+*/
