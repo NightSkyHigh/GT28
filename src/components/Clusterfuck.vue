42111ca8b9dd69b0d1a76f546e9cb8c1dc55bb0e
kaaasen <kim.andre.aasen@gmail.com>
2019-03-27 10:03:08 +0100

    Added frames for looks

--- src/components/Clusterfuck.vue
+++ src/components/Clusterfuck.vue
@@ -13,27 +13,37 @@
     <v-list dense>
       <v-list-tile @click="">
         <v-list-tile-content>
-          <v-list-tile-title><a href="#" v-on:click="setCoords(52.52, 13.40, 9)">Berlin</a></v-list-tile-title>
+          <v-list-tile-title><a href="#" v-on:click="setCoords(52.21, 21.00, 4.2)" @click.stop="drawer = !drawer">Reset map</a></v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
       <v-list-tile @click="">
         <v-list-tile-content>
-          <v-list-tile-title><a href="#" v-on:click="setCoords(51.50, -0.11, 9)">London</a></v-list-tile-title>
+          <v-list-tile-title><a href="#" v-on:click="setCoords(52.52, 13.40, 9)" @click.stop="drawer = !drawer">Berlin, Germany</a></v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
       <v-list-tile @click="">
         <v-list-tile-content>
-          <v-list-tile-title><a href="#" v-on:click="setCoords(55.75, 37.61, 9)">Moscow</a></v-list-tile-title>
+          <v-list-tile-title><a href="#" v-on:click="setCoords(51.50, -0.11, 9)" @click.stop="drawer = !drawer">London, England</a></v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
       <v-list-tile @click="">
         <v-list-tile-content>
-          <v-list-tile-title><a href="#" v-on:click="setCoords(59.91, 10.74, 9)">Oslo</a></v-list-tile-title>
+          <v-list-tile-title><a href="#" v-on:click="setCoords(55.75, 37.61, 9)" @click.stop="drawer = !drawer">Moscow, Russia</a></v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
       <v-list-tile @click="">
         <v-list-tile-content>
-          <v-list-tile-title><a href="#" v-on:click="setCoords(45.86, 25.78, 12)">Sfântu-Gheorghe</a></v-list-tile-title>
+          <v-list-tile-title><a href="#" v-on:click="setCoords(59.91, 10.74, 9)" @click.stop="drawer = !drawer">Oslo, Norway</a></v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile @click="">
+        <v-list-tile-content>
+          <v-list-tile-title><a href="#" v-on:click="setCoords(-33.96, 25.58, 12)" @click.stop="drawer = !drawer">Port Elizabeth, South Africa</a></v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile @click="">
+        <v-list-tile-content>
+          <v-list-tile-title><a href="#" v-on:click="setCoords(45.86, 25.78, 12)" @click.stop="drawer = !drawer">Sfântu-Gheorghe, Romania</a></v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
     </v-list>
@@ -42,7 +52,24 @@
       <v-container fluid >
         <v-layout justify-left align-left>
           <v-flex text-xs-center>
-            <div id="mapid"></div>
+            <div id="infoAndMapContainer">
+              <h3>Forecast</h3>
+              <div id="infoContainer">
+                <div id="leftInfo">
+                  <p>Yesterday:</p>
+                  <img src="../images/logo.png" height="50px" width="50px"></img>
+                </div>
+                <div id="middleInfo">
+                  <p>Today:</p>
+                  <img src="../images/logo.png" height="50px" width="50px"></img>
+                </div>
+                <div id="rightInfo">
+                  <p>Tomorrow:</p>
+                  <img src="../images/logo.png" height="50px" width="50px"></img>
+                </div>
+              </div>
+              <div id="mapid"></div>
+            </div>
           </v-flex>
         </v-layout>
       </v-container>
@@ -58,7 +85,7 @@ export default {
   data: () => ({
     drawer: null,
     maxZoom: 18,
-    id: 'mapbox.streets',
+    id: null,
     accessToken: 'pk.eyJ1Ijoia2FhYXNlbiIsImEiOiJjanRwZ2tmY2YwMDJnNGRxZGplMHZtdDJ3In0.TzTQ3C8LMcW8A1LIgyNYBg',
     map: null,
     tileLayer: null,
@@ -75,7 +102,7 @@ export default {
 
         this.tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
           maxZoom: 18,
-          id: 'mapbox.light',
+          id: 'mapbox.streets',
           accessToken: 'pk.eyJ1Ijoia2FhYXNlbiIsImEiOiJjanRwZ2tmY2YwMDJnNGRxZGplMHZtdDJ3In0.TzTQ3C8LMcW8A1LIgyNYBg'
         })
         this.tileLayer.addTo(this.map);
@@ -84,6 +111,7 @@ export default {
         var londonMarker = L.marker([51.50, -0.11]).addTo(this.map);
         var moscowMarker = L.marker([55.75, 37.61]).addTo(this.map);
         var osloMarker = L.marker([59.91, 10.74]).addTo(this.map);
+        var portelizabethMarker = L.marker([-33.96, 25.58]).addTo(this.map);
         var sfântugheorgheMarker = L.marker([45.86, 25.78]).addTo(this.map);
       },
       setCoords: function(lat, long, zoom) {
@@ -91,7 +119,7 @@ export default {
 
         this.tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
           maxZoom: 18,
-          id: 'mapbox.light',
+          id: 'mapbox.streets',
           accessToken: 'pk.eyJ1Ijoia2FhYXNlbiIsImEiOiJjanRwZ2tmY2YwMDJnNGRxZGplMHZtdDJ3In0.TzTQ3C8LMcW8A1LIgyNYBg'
         })
         this.tileLayer.addTo(this.map);
@@ -104,12 +132,45 @@ export default {
 </script>
 
 <style>
+#infoAndMapContainer {
+  display: flex;
+  flex-flow: row wrap;
+  margin: 0 0 0 0;
+}
+
+#infoContainer {
+  height: 100px;
+  width: 800px;
+  margin-bottom: 2px;
+  flex: 1 100%;
+  opacity: 0.8;
+  display: flex;
+}
+
+#info {
+  height: 200px;
+  width: 800px;
+  border: 2px solid #0288D1;
+  border-radius: 12px;
+  flex: 1 100%;
+  border: 2px solid #0288D1;
+  border-radius: 12px;
+}
+
+#leftInfo, #middleInfo, #rightInfo {
+  border: 2px solid #0288D1;
+  border-radius: 12px;
+  flex: 1 100%;
+  margin: 2px;
+}
+
 #mapid {
-  height: 500px;
+  height: 400px;
   width: 800px;
   z-index: 1;
   border: 2px solid #0288D1;
   border-radius: 12px;
+  flex: 1 100%;
 }
 
 #logo {
@@ -127,5 +188,13 @@ a {
 a:hover {
   color: #000000;
   cursor: pointer;
+}
+
+h3 {
+  font-size: 20px;
+}
+
+p {
+  font-weight: bold;
 }
 </style>
